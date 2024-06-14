fb9f827caf134ce4e209b4c9a8f0f440762c1a55
mintkat <lucasbear9@gmail.com>
2024-06-14 06:21:07 -0800

    Init

---
+++ AvaloniaJs/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaJs.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AvaloniaJs/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaJs.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
